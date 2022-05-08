580014627687058965823707ad5dafa2daae1df3
Nikita Terpilovsky <work.terpilovsky@gmail.com>
2022-05-08 19:32:19 +0300

    First commit

---
+++ Accurate output of seconds/Accurate output of seconds.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    int sec;
+    float days, hours, mins, secs;
+
+    cout << "Enter number of seconds: ";
+    cin >> sec;
+
+    days = sec / 86400.0;
+    hours = (days - trunc(days)) * 24;
+    mins = (hours - trunc(hours)) * 60;
+    secs = (mins - trunc(mins)) * 60;
+    cout << "It is " << trunc(days) << " days " << trunc(hours) << " hours " << trunc(mins) << " minutes " << round(secs) << " seconds " << endl;
+}
---
+++ Accurate weight output/Accurate weight output.cpp
@@ -0,0 +1,16 @@
+﻿#include <iostream>
+using namespace std;
+
+int main()
+{
+    double tones;
+    float kilograms, grams;
+
+    cout << "Enter the number of tones: ";
+    cin >> tones;
+
+    kilograms = (tones - (int)tones) * 1000;
+    grams = (kilograms - (int)kilograms) * 1000;
+
+    cout << "It is " << (int)tones << " tones, " << (int)kilograms << " kilograms, " << (int)grams << " grams " << endl;
+}
